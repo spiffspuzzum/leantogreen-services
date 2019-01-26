7c418217b4e62a62009fce5eb47608bd9de3ebb0
Robert Norton <spiffspuzzum@users.noreply.github.com>
2019-01-25 19:44:43 -0800

    added x-auth-token to allowed headers

--- startup/prod.js
+++ startup/prod.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     //Allow CORS
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
         next();
     });
 };
