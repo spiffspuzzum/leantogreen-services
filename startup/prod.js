9f9907590c29d2b68246cae57efb1d9b9096e7bf
Robert Norton <spiffspuzzum@users.noreply.github.com>
2019-01-26 16:28:09 -0800

    added allowed methods to header

--- startup/prod.js
+++ startup/prod.js
@@ -6,6 +6,7 @@ module.exports = function(app) {
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, OPTIONS');
         next();
     });
 };
